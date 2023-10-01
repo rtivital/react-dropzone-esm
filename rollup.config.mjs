2c856245bdce192da4e8ebb75d2d48416dc3c0cf
Vitaly Rtishchev <rtivital@gmail.com>
2023-10-01 18:05:23 +0400

    feat: update rollup configuration for new esm and cjs formats

---
+++ rollup.config.mjs
@@ -0,0 +1,44 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeExternals from "rollup-plugin-node-externals";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import banner from "rollup-plugin-banner2";
+import esbuild from "rollup-plugin-esbuild";
+
+export default {
+  input: "./src/index.jsx",
+  output: [
+    {
+      dir: "./dist/cjs",
+      preserveModules: true,
+      exports: "named",
+      format: "cjs",
+    },
+    {
+      dir: "./dist/esm",
+      preserveModules: true,
+      entryFileNames: "[name].mjs",
+      format: "esm",
+    },
+  ],
+
+  plugins: [
+    esbuild({
+      // minify: true,
+      // jsx: "transform",
+      sourceMap: false,
+      target: "es2015",
+    }),
+    nodeExternals(),
+    nodeResolve(),
+    commonjs(),
+    replace({ preventAssignment: true }),
+    banner((chunk) => {
+      if (chunk.fileName === "index.js" || chunk.fileName === "index.mjs") {
+        return "'use client';\n";
+      }
+
+      return undefined;
+    }),
+  ],
+};
